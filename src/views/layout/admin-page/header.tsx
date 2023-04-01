04dcbdebcd189115915d8678411c16d3698fd170
Ahmed Ghonim <ahmed.ghonim@g2k-group.com>
2023-04-01 19:36:40 +0200

    fix: 🚀 header

--- src/views/layout/admin-page/header.tsx
+++ src/views/layout/admin-page/header.tsx
@@ -1,10 +1,12 @@
 import useTranslation from "next-translate/useTranslation";
-import { useSelectedLayoutSegments } from "next/navigation";
+import { useRouter } from "next/router";
 import React from "react";
 
 function AdminHeader() {
   const { t } = useTranslation("pages-title");
-  return <h1 className="text-[28px] font-bold">t</h1>;
+  const { asPath } = useRouter();
+  const currentPath = asPath.split("/").reverse()[0];
+  return <h1 className="text-[28px] font-bold">{t(currentPath)}</h1>;
 }
 
 export default AdminHeader;
